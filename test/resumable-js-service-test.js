74e5c4e67ec572f90261f4b2f9fa0b3e2934ff06
Zachary Chavez <zach@synapsestudios.com>
2016-05-06 16:55:55 -0700

    Test chunkPrefix

--- test/resumable-js-service-test.js
+++ test/resumable-js-service-test.js
@@ -44,6 +44,35 @@ describe('ResumableJsService', () => {
             return expect(promise).to.be.rejected;
         });
 
+        it('uses chunkPrefix set with setChunkPrefix in chunk filename', () => {
+            ResumableJsService.__Rewire__('fs', {
+                exists: () => new Promise(resolve => resolve(true)),
+            });
+            var service = new ResumableJsService();
+            var request = getValidRequest('GET');
+
+            service.setChunkPrefix('chunk-prefix');
+            var promise = service.get(request);
+
+            return expect(promise).to.be.fulfilled.then(result => {
+                expect(result.chunkFilename).to.match(/\/chunk-prefix/);
+            });
+        });
+
+        it('uses chunkPrefix set in constructor in chunk filename', () => {
+            ResumableJsService.__Rewire__('fs', {
+                exists: () => new Promise(resolve => resolve(true)),
+            });
+            var service = new ResumableJsService({ chunkPrefix: 'chunk-prefix' });
+            var request = getValidRequest('GET');
+
+            var promise = service.get(request);
+
+            return expect(promise).to.be.fulfilled.then(result => {
+                expect(result.chunkFilename).to.match(/\/chunk-prefix/);
+            });
+        });
+
         it('rejects if chunkNumber is < 1', () => {
             var service = new ResumableJsService();
             var request = getValidRequest('GET');
@@ -214,6 +243,41 @@ describe('ResumableJsService', () => {
             return expect(promise).to.be.fulfilled.then(result => {
                 expect(result.complete).to.equal(true);
                 expect(result.identifier).to.equal(service._cleanIdentifier(request.payload.resumableIdentifier));
+            });
+        });
+
+        it('uses chunkPrefix set with setChunkPrefix in chunk filename', () => {
+            var mockFs = {
+                rename: () => new Promise(resolve => resolve()),
+                exists: () => new Promise(resolve => resolve(true)),
+            };
+            sinon.spy(mockFs, 'rename');
+            ResumableJsService.__Rewire__('fs', mockFs);
+            var service = new ResumableJsService();
+            var request = getValidRequest('POST');
+
+            service.setChunkPrefix('chunk-prefix');
+            var promise = service.post(request);
+
+            return expect(promise).to.be.fulfilled.then(result => {
+                expect(mockFs.rename.args[0][1]).to.match(/\/chunk-prefix/);
+            });
+        });
+
+        it('uses chunkPrefix set in constructor in chunk filename', () => {
+            var mockFs = {
+                rename: () => new Promise(resolve => resolve()),
+                exists: () => new Promise(resolve => resolve(true)),
+            };
+            sinon.spy(mockFs, 'rename');
+            ResumableJsService.__Rewire__('fs', mockFs);
+            var service = new ResumableJsService({ chunkPrefix: 'chunk-prefix' });
+            var request = getValidRequest('POST');
+
+            var promise = service.post(request);
+
+            return expect(promise).to.be.fulfilled.then(result => {
+                expect(mockFs.rename.args[0][1]).to.match(/\/chunk-prefix/);
             });
         });
     });
